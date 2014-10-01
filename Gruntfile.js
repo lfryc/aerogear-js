1ca5ea04abfbaf803f8c67aa03c5ef66b2534f36
Lukas Fryc <lukas@fryc.eu>
2014-10-01 11:11:49 +0200

    use microlib

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,25 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
+        cfg: {
+            barename: 'aerogear.core',
+            namespace: 'AeroGear'
+        },
         transpile: {
             core: {}
+        },
+        microlib: {
+            core: {
+                files: [ { src: 'dist/aerogear.core.amd.js', dest: 'dist/aerogear.core.micro.js' } ]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-es6-module-transpiler');
+    grunt.loadNpmTasks('grunt-microlib');
     grunt.loadTasks('tasks');
+    grunt.renameTask('browser', 'microlib');
 
-    grunt.registerTask('default', ['transpile']);
+    grunt.registerTask('default', ['transpile', 'microlib']);
 
 };
