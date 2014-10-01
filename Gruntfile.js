f104e0d5f59ef38694b67a66ab747ee966d99202
Lukas Fryc <lukas@fryc.eu>
2014-10-01 10:52:19 +0200

    compile using custom transpile task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,13 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    transpile: {
-      main: {
-        type: "amd", // or "amd" or "yui"
-        files: [{
-          expand: true,
-          src: ['src/aerogear.core.js'],
-          dest: 'dist/'
-        }]
+      transpile: {
+          core: {}
       }
-    }
   });
 
   grunt.loadNpmTasks('grunt-es6-module-transpiler');
+  grunt.loadTasks('tasks');
 
   grunt.registerTask('default', ['transpile']);
 
